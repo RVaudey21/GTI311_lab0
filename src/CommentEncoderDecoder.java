3b2480209ef3eded668927b26653bb525b7d3d09
RVaudey <remy.vaudey@insa-lyon.fr>
2025-01-25 12:08:55 -0500

    encode_v1

---
+++ src/CommentEncoderDecoder.java
@@ -0,0 +1,26 @@
+
+public class CommentEncoderDecoder {
+
+    // Méthode pour encoder un commentaire
+    public static byte[] encodeComment (String comment, int delay) {
+
+        byte[] commentbytes = comment.getBytes();
+
+
+        return commentbytes;
+    }
+
+    // Méthode pour décoder le commentaire
+    public static String decodeComment(byte[] encodedComment) {
+        
+        String comment = encodedComment.toString();
+
+        return comment;
+    }
+
+    public static void main(String[] args) {
+        String comment = "Bonjour 123";
+        byte[] encodedComment = encodeComment(comment, 0);
+        System.out.println(encodedComment); 
+    }
+}
